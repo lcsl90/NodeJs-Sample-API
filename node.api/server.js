d8b8dde43178faef462cd3773f7ba1928f670efd
Lucas <lucas.lietz@gmx.de>
2019-07-20 02:26:58 +0200

    Prep for protected and unprotected routes

--- node.api/server.js
+++ node.api/server.js
@@ -1,15 +1,19 @@
 const express       = require('express');
-//const sqlite3       = require('sqlite3');
-const bodyParser    = require('body-parser');
-const nodemon       = require('nodemon');
+var bodyParser = require("body-parser");
 
+//Init stuff
 const app = express();
 const port = 1337;
 
-app.use(bodyParser.urlencoded({extended:true}));
-require('./app/routes')(app, {});
+//Import Routes
+const authRoute  = require('./api-routes/auth');
+const nodeRoutes = require('./routes/node_routes');
 
-app.listen(port, () =>{
-    console.log("We live, boyyyyys. Port: " + port); 
-})
+//route mw
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/api', authRoute);
+app.use(nodeRoutes);
+
+app.listen(port, () => console.log("We live, boyyyyys. Port: " + port));
